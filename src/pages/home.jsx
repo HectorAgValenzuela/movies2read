962a1a18306a32e504147356bafde6f5ac1fe7c9
HectorAgValenzuela <hector-av1@hotmail.com>
2022-12-04 11:59:14 -0700

    agregando barra de busqueda

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,21 @@
 const Home = () => {
     return(
         <div>
-            Home
+           
+      <div class="newsletter_section layout_padding">
+         <div class="container">
+            <h1 class="newsletter_text">Subscribe Our Newsletter</h1>
+            <div class="box_main">
+               <p class="dummy_text">It is a long established fact that a reader will be distracted by the readable content of a page when looking</p>
+               <div class="mail_main">
+                  <input type="text" class="email_text" placeholder="Enter Your email" name="Enter Your email" />
+                  <div class="right_arrow">
+                     <a href="#"><img src="images/right-arrow.png" /></a>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
         </div>
     )
 }
