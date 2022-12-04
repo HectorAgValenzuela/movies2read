437c2d15a1175a6b54aa787bb807c7cb0fd0170a
HectorAgValenzuela <hector-av1@hotmail.com>
2022-12-04 16:46:55 -0700

    Agregando los datos de películas al dropdown

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,26 +1,17 @@
 import { useState } from "react";
 import { MultiSelect } from "react-multi-select-component";
-import { Link, useNavigate } from "react-router-dom";
-import data from 'items/interfaceMovieId,json'
-
-const options = [
-    { label: "Grapes", value: "2" },
-    { label: "Mango", value: "8" },
-    { label: "Strawberry", value: "5" }, 
-  ];
+import { useNavigate } from "react-router-dom";
+import data from '../items/interfaceMovieId.json'
   
-
 const Home = () => {
-
-
-    console.log(data)
 
     const [selected, setSelected] = useState([]);
     const [books, setBooks] = useState("")
     const navigate = useNavigate()
 
+
     const redirect = () => {
-        setBooks(selected.map((book) => book.value).join(" "))
+        setBooks(selected.map((book) => book.value).join("-"))
         navigate(`/${books}`)
     }
 
@@ -33,7 +24,7 @@ const Home = () => {
                         <p class="dummy_text">Dinos qué géneros de películas son tus favoritos</p>
                         <div class="mail_main">
                             <MultiSelect
-                                options={options}
+                                options={data}
                                 value={selected}
                                 onChange={setSelected}
                                 labelledBy="Selected"
