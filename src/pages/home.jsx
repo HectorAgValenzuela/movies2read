e889fc1e2cc3090987dc20a43c675de4115c47cd
HectorAgValenzuela <hector-av1@hotmail.com>
2022-12-04 13:11:54 -0700

    Agregando dropdown con datos dummies

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,21 +1,35 @@
+import { useState } from "react";
+import { MultiSelect } from "react-multi-select-component";
+
+const options = [
+    { label: "Grapes 🍇", value: "grapes" },
+    { label: "Mango 🥭", value: "mango" },
+    { label: "Strawberry 🍓", value: "strawberry", disabled: true },
+  ];
+  
+
 const Home = () => {
+    const [selected, setSelected] = useState([]);
+
     return(
         <div>
-           
-      <div class="newsletter_section layout_padding">
-         <div class="container">
-            <h1 class="newsletter_text">Subscribe Our Newsletter</h1>
-            <div class="box_main">
-               <p class="dummy_text">It is a long established fact that a reader will be distracted by the readable content of a page when looking</p>
-               <div class="mail_main">
-                  <input type="text" class="email_text" placeholder="Enter Your email" name="Enter Your email" />
-                  <div class="right_arrow">
-                     <a href="#"><img src="images/right-arrow.png" /></a>
-                  </div>
-               </div>
+            <div class="newsletter_section layout_padding">
+                <div class="container">
+                    <h1 class="newsletter_text">¡Dinos tus gustos!</h1>
+                    <div class="box_main">
+                    <p class="dummy_text">Dinos qué géneros de películas son tus favoritos</p>
+                    <div class="mail_main">
+                        <MultiSelect
+                            options={options}
+                            value={selected}
+                            onChange={setSelected}
+                            labelledBy="Selected"
+                            className="multi-select"
+                        />
+                    </div>
+                    </div>
+                </div>
             </div>
-         </div>
-      </div>
         </div>
     )
 }
