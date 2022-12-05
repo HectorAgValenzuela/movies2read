4054743d0bfdb767229505bb590ad42ab8883ce8
HectorAgValenzuela <hector-av1@hotmail.com>
2022-12-04 17:42:18 -0700

    Añadiendo template a componente de recomendaciones

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,9 +1,114 @@
 const Books = () => {
-    
+
 
     return (
         <div>
-            Books
+            <div class="movies_section layout_padding">
+                <div class="container">
+                    <div class="movies_section_2 layout_padding">
+                    <h2 class="letest_text">Recomendaciones</h2>
+                        <div class="movies_main">
+                            <div class="iamge_movies_main">
+                                <div class="iamge_movies">
+                                    <div class="image_3">
+                                        <img src="assets/images/img-3.png" class="image" alt=""/>
+                                        <div class="middle">
+                                            <div class="playnow_bt">Play Now</div>
+                                        </div>
+                                    </div>
+                                    <h1 class="code_text">CADE Prlor</h1>
+                                    <p class="there_text">There are many variations </p>
+                                    <div class="star_icon">
+                                        <ul>
+                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
+                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
+                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
+                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
+                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="iamge_movies">
+                                    <div class="image_3">
+                                        <img src="assets/images/img-4.png" class="image" alt=""/>
+                                        <div class="middle">
+                                            <div class="playnow_bt">Play Now</div>
+                                        </div>
+                                    </div>
+                                    <h1 class="code_text">CADE Prlor</h1>
+                                    <p class="there_text">There are many variations </p>
+                                    <div class="star_icon">
+                                        <ul>
+                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
+                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
+                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
+                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
+                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="iamge_movies">
+                                    <div class="image_3">
+                                        <img src="assets/images/img-5.png" class="image" alt=""/>
+                                        <div class="middle">
+                                            <div class="playnow_bt">Play Now</div>
+                                        </div>
+                                    </div>
+                                    <h1 class="code_text">CADE Prlor</h1>
+                                    <p class="there_text">There are many variations </p>
+                                    <div class="star_icon">
+                                        <ul>
+                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
+                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
+                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
+                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
+                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="iamge_movies">
+                                    <div class="image_3">
+                                        <img src="assets/images/img-6.png" class="image" alt=""/>
+                                        <div class="middle">
+                                            <div class="playnow_bt">Play Now</div>
+                                        </div>
+                                    </div>
+                                    <h1 class="code_text">CADE Prlor</h1>
+                                    <p class="there_text">There are many variations </p>
+                                    <div class="star_icon">
+                                        <ul>
+                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
+                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
+                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
+                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
+                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="iamge_movies">
+                                    <div class="image_3">
+                                        <img src="assets/images/img-7.png" class="image" alt=""/>
+                                        <div class="middle">
+                                            <div class="playnow_bt">Play Now</div>
+                                        </div>
+                                    </div>
+                                    <h1 class="code_text">CADE Prlor</h1>
+                                    <p class="there_text">There are many variations </p>
+                                    <div class="star_icon">
+                                        <ul>
+                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
+                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
+                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
+                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
+                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { MultiSelect } from "react-multi-select-component";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import data from '../items/interfaceMovieId.json'
   
 const Home = () => {
@@ -19,9 +19,9 @@ const Home = () => {
         <div>
             <div class="newsletter_section layout_padding">
                 <div class="container">
-                    <h1 class="newsletter_text">¡Dinos tus gustos!</h1>
+                    <h1 class="newsletter_text">¡Selecciona tus películas favoritas!</h1>
                     <div class="box_main">
-                        <p class="dummy_text">Dinos qué géneros de películas son tus favoritos</p>
+                        <p class="dummy_text">Dinos qué películas son tus favoritos</p>
                         <div class="mail_main">
                             <MultiSelect
                                 options={data}
@@ -31,7 +31,8 @@ const Home = () => {
                                 className="multi-select"
                             />
                         </div>
-                        <button onClick={redirect} className="btn btn-primary mt-5">Recomiendame</button>
+                        <button onClick={redirect} className="btn btn-primary mt-5">Recomiéndame</button>
+                        <Link to={'/books'}>Books</Link>
                     </div>
                 </div>
             </div>
