0a63a81984860108d6b82e27ed439736bfb76223
HectorAgValenzuela <hector-av1@hotmail.com>
2022-12-05 15:38:29 -0700

    Arreglando layout de books

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -5,10 +5,10 @@ const Books = () => {
         <div>
             <div class="movies_section layout_padding">
                 <div class="container">
-                    <div class="movies_section_2 layout_padding">
                     <h2 class="letest_text">Recomendaciones</h2>
                         <div class="movies_main">
                             <div class="iamge_movies_main">
+
                                 <div class="iamge_movies">
                                     <div class="image_3">
                                         <img src="assets/images/img-3.png" class="image" alt=""/>
@@ -18,16 +18,8 @@ const Books = () => {
                                     </div>
                                     <h1 class="code_text">CADE Prlor</h1>
                                     <p class="there_text">There are many variations </p>
-                                    <div class="star_icon">
-                                        <ul>
-                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
-                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
-                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
-                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
-                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
-                                        </ul>
-                                    </div>
                                 </div>
+
                                 <div class="iamge_movies">
                                     <div class="image_3">
                                         <img src="assets/images/img-4.png" class="image" alt=""/>
@@ -37,16 +29,8 @@ const Books = () => {
                                     </div>
                                     <h1 class="code_text">CADE Prlor</h1>
                                     <p class="there_text">There are many variations </p>
-                                    <div class="star_icon">
-                                        <ul>
-                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
-                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
-                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
-                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
-                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
-                                        </ul>
-                                    </div>
                                 </div>
+
                                 <div class="iamge_movies">
                                     <div class="image_3">
                                         <img src="assets/images/img-5.png" class="image" alt=""/>
@@ -56,16 +40,8 @@ const Books = () => {
                                     </div>
                                     <h1 class="code_text">CADE Prlor</h1>
                                     <p class="there_text">There are many variations </p>
-                                    <div class="star_icon">
-                                        <ul>
-                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
-                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
-                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
-                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
-                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
-                                        </ul>
-                                    </div>
                                 </div>
+
                                 <div class="iamge_movies">
                                     <div class="image_3">
                                         <img src="assets/images/img-6.png" class="image" alt=""/>
@@ -75,16 +51,8 @@ const Books = () => {
                                     </div>
                                     <h1 class="code_text">CADE Prlor</h1>
                                     <p class="there_text">There are many variations </p>
-                                    <div class="star_icon">
-                                        <ul>
-                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
-                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
-                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
-                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
-                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
-                                        </ul>
-                                    </div>
                                 </div>
+
                                 <div class="iamge_movies">
                                     <div class="image_3">
                                         <img src="assets/images/img-7.png" class="image" alt=""/>
@@ -94,19 +62,9 @@ const Books = () => {
                                     </div>
                                     <h1 class="code_text">CADE Prlor</h1>
                                     <p class="there_text">There are many variations </p>
-                                    <div class="star_icon">
-                                        <ul>
-                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
-                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
-                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
-                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
-                                            <li><a href="#"><img src="images/star-icon.png" alt=""/></a></li>
-                                        </ul>
-                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
                 </div>
             </div>
         </div>
