2ddeb8135e4f14242f1af59cda21188dd37183b9
HectorAgValenzuela <hector-av1@hotmail.com>
2022-12-07 19:01:31 -0700

    router para gh

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
 import Books from './components/Books';
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/:movies' element={<Books/>} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
